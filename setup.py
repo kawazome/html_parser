8bf0bd80c6b50a9919a55ce90f1c4a3cf1a4c5a8
kawazome <id.kawazome@gmail.com>
2022-10-29 16:45:54 +0900

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='html_parser',
     version="1.0.1",
-    description="Utility class to easily manipulate image files.",
+    description="Utility class to easily parse html files.",
     long_description="",
     author='kawazome',
     packages=find_packages()
